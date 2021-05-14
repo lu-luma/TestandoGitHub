f10087a68d381cda803c91afa3b8fa795d0a6a1e
lu-luma <lumaluizalua@gmail.com>
2021-05-14 08:53:21 -0300

    -Coloquei o meu nome no MainActivity

--- app/src/main/java/com/example/testandogithub/MainActivity.java
+++ app/src/main/java/com/example/testandogithub/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.testandogithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//Luiza Maria da Silva(SC1901354) 3Ano TII
 public class MainActivity extends AppCompatActivity {
 
     @Override
